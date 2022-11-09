3a2a1518193b93973c4e28d225a902a9d1d48211
KKuTuIO <system@kkutu.io>
2022-11-09 10:56:46 +0000

    최신 게임 서버 소스 코드 업데이트

--- Server/sub/db.js
+++ Server/sub/db.js
@@ -75,8 +75,6 @@ export let MANNER_CACHE;
 export let SPC_MANNER_CACHE;
 export let THEME_CACHE;
 export let shop;
-export let refreshWordcache;
-export let refreshShopcache;
 
 let ready;
 
@@ -114,164 +112,7 @@ function connectPg(noRedis) {
         THEME_CACHE = {'ko': {}, 'en': {}}
         shop = {};
 
-        refreshWordcache = function() {
-            IOLog.info(`단어 데이터를 메모리에 저장합니다...`)
-
-            kkutu['ko'].find(['type', KOR_GROUP]).on($res => {
-                let newCache = {};
-                let newManner = {};
-                let newSpcManner = {};
-                let newTheme = {};
-                for (let resIndex in $res) {
-                    const data = $res[resIndex];
-                    const _id = data['_id'];
-                    const flag = data['flag'];
-                    const theme = data['theme'].split(',');
-                    const hbw = theme.includes('HBW');
-                    
-                    newCache[_id] = data;
-
-                    // 주제별 캐싱, 선택 불가능 주제는 기록하지 않음
-                    for (let t of theme) {
-                        if (IJP_EXCEPT.includes(t)) continue;
-                        if (!newTheme.hasOwnProperty(t)) newTheme[t] = [];
-                        newTheme[t].push(data);
-                    }
-
-                    if (!data['type'].match(KOR_GROUP)) continue; // 클래식에서 사용 불가능한 단어
-
-                    if (_id.length < 2) continue;
-                    const start = _id.charAt(_id.length-1); // 시작하는 글자
-                    const last = _id.charAt(); // 끝나는 글자
-
-                    if (start == last || start == getSubKorean(last)) continue; // 글자돌림, 매너 테이블에서 계산 안함
-
-                    /*  인덱스 순서대로, * = 매너 계산 적용받지 않음
-                     *  0 = 일반 단어
-                     *  1 = 깐깐에서 사용 불가능한 단어
-                     *  2 = 우리말에서 사용 불가능한 단어
-                     *  3 = HBW 주제의 단어
-                     *  4 = *HBW 주제 외의 어인정 단어
-                     */
-                    let fi = 0;
-                    if (!hbw && (flag & 2)) fi = 4;
-                    else if (hbw) fi = 3;
-                    else if (flag & 1) fi = 2;
-                    else if (flag > 3) fi = 1;
-
-                    if (!(flag & 64)) { // 쿵쿵따 전용 단어 스킵
-
-                        if (!newManner.hasOwnProperty(start)) newManner[start] = [getMannerTemplate(5), getMannerTemplate(5)];
-                        if (!newManner.hasOwnProperty(last)) newManner[last] = [getMannerTemplate(5), getMannerTemplate(5)];
-
-                        newManner[start][1][fi].push(data);
-                        newManner[last][0][fi].push(data);
-                    }
-
-                    if (_id.length > 3) continue; // 2, 3글자일때는 쿵쿵따 테이블 기록
-
-                    let i = 0;
-                    if (_id.length == 2) i = 1; // 3232용 2글자 기록
-
-                    if (!newSpcManner.hasOwnProperty(last)) newSpcManner[last] = [getMannerTemplate(5), getMannerTemplate(5)];
-                    newSpcManner[last][i][fi].push(data);
-
-                }
-
-                SUBMIT_WORD_CACHE['ko'] = newCache;
-                MANNER_CACHE['ko'] = newManner;
-                SPC_MANNER_CACHE['ko'] = newSpcManner;
-                THEME_CACHE['ko'] = newTheme;
-
-                IOLog.info(`${Object.keys(SUBMIT_WORD_CACHE['ko']).length} 개의 한국어 단어 데이터를 메모리에 불러왔습니다.`)
-                IOLog.debug(`${Object.keys(MANNER_CACHE['ko']).length} 개의 한국어 일반 매너 데이터를 메모리에 불러왔습니다.`)
-                IOLog.debug(`${Object.keys(SPC_MANNER_CACHE['ko']).length} 개의 한국어 특수 매너 데이터를 메모리에 불러왔습니다.`)
-                IOLog.debug(`${Object.keys(THEME_CACHE['ko']).length} 개의 한국어 주제 데이터를 메모리에 불러왔습니다.`)
-            });
-
-            kkutu['en'].find(['_id', ENG_ID]).on($res => {
-                let newCache = {};
-                let newManner = {};
-                let newSpcManner = {};
-                let newTheme = {};
-                for (let resIndex in $res) {
-                    const data = $res[resIndex];
-                    const _id = data['_id'];
-                    const flag = data['flag'];
-                    const theme = data['theme'].split(',');
-                    
-                    newCache[_id] = data;
-
-                    // 주제별 캐싱, 선택 불가능 주제는 기록하지 않음
-                    for (let t of theme) {
-                        if (IJP_EXCEPT.includes(t)) continue;
-                        if (!newTheme.hasOwnProperty(t)) newTheme[t] = [];
-                        newTheme[t].push(data);
-                    }
-
-                    if (!_id.match(ENG_ID)) continue; // 클래식에서 사용 불가능한 단어
-
-                    if (_id.length < 2) continue; // 끝말/앞말
-                    const start = _id.charAt(_id.length-1); // 시작하는 글자
-                    const last = _id.charAt(); // 끝나는 글자
-
-                    /*  인덱스 순서대로, * = 매너 계산 적용받지 않음
-                     *  0 = 일반 단어
-                     *  1 = *어인정 단어
-                     */
-                    let fi = 0;
-                    if (flag & 2) fi = 1;
-
-                    if (!newManner.hasOwnProperty(start)) newManner[start] = [getMannerTemplate(2), getMannerTemplate(2)];
-                    if (!newManner.hasOwnProperty(last)) newManner[last] = [getMannerTemplate(2), getMannerTemplate(2)];
-
-                    if (start != last) { // 글자돌림 제외
-                        newManner[start][1][fi].push(data);
-                        newManner[last][0][fi].push(data);
-                    }
-
-                    if (_id.length < 4) continue; // 4글자 이상이면 끄투 규칙용 데이터도 저장
-                    const kkchar = _id.slice(0,3);
-                    const kksub = _id.slice(0,2);
-
-                    if (!newSpcManner.hasOwnProperty(kkchar)) newSpcManner[kkchar] = getMannerTemplate(2);
-                    if (!newSpcManner.hasOwnProperty(kksub)) newSpcManner[kksub] = getMannerTemplate(2);
-
-                    newSpcManner[kkchar][fi].push(data);
-                    newSpcManner[kksub][fi].push(data);
-                }
-
-                SUBMIT_WORD_CACHE['en'] = newCache;
-                MANNER_CACHE['en'] = newManner;
-                SPC_MANNER_CACHE['en'] = newSpcManner;
-                THEME_CACHE['en'] = newTheme;
-
-                IOLog.info(`${Object.keys(SUBMIT_WORD_CACHE['en']).length} 개의 영어 단어 데이터를 메모리에 불러왔습니다.`)
-                IOLog.debug(`${Object.keys(MANNER_CACHE['en']).length} 개의 영어 일반 매너 데이터를 메모리에 불러왔습니다.`)
-                IOLog.debug(`${Object.keys(SPC_MANNER_CACHE['en']).length} 개의 영어 특수 매너 데이터를 메모리에 불러왔습니다.`)
-                IOLog.debug(`${Object.keys(THEME_CACHE['en']).length} 개의 영어 주제 데이터를 메모리에 불러왔습니다.`)
-            });
-        };
-
         refreshWordcache();
-
-
-        refreshShopcache = function() {    
-            IOLog.info('아이템 데이터 갱신을 시작합니다...')
-
-            kkutu_shop.find().on(function ($res) {
-                let newCache = {};
-    
-                $res.forEach(function (item) {
-                    newCache[item._id] = item;
-                });
-    
-                shop = newCache;
-
-                IOLog.info(`${Object.keys(shop).length} 개의 아이템 데이터를 메모리에 불러왔습니다.`)
-            });
-        };
-
         refreshShopcache();
 
         ConnectionLog.initDatabase(pgMain);
@@ -325,3 +166,158 @@ function getMannerTemplate (len) {
     for (let i = 0;i < len;i++) R.push(new Array())
     return R;
 }
+
+export function refreshWordcache () {
+    IOLog.info(`단어 데이터를 메모리에 저장합니다...`)
+
+    kkutu['ko'].find(['type', KOR_GROUP]).on($res => {
+        let newCache = {};
+        let newManner = {};
+        let newSpcManner = {};
+        let newTheme = {};
+        for (let resIndex in $res) {
+            const data = $res[resIndex];
+            const _id = data['_id'];
+            const flag = data['flag'];
+            const theme = data['theme'].split(',');
+            const hbw = theme.includes('HBW');
+            
+            newCache[_id] = data;
+
+            // 주제별 캐싱, 선택 불가능 주제는 기록하지 않음
+            for (let t of theme) {
+                if (IJP_EXCEPT.includes(t)) continue;
+                if (!newTheme.hasOwnProperty(t)) newTheme[t] = [];
+                newTheme[t].push(data);
+            }
+
+            if (!data['type'].match(KOR_GROUP)) continue; // 클래식에서 사용 불가능한 단어
+
+            if (_id.length < 2) continue;
+            const start = _id.charAt(_id.length-1); // 시작하는 글자
+            const last = _id.charAt(); // 끝나는 글자
+
+            if (start == last || start == getSubKorean(last)) continue; // 글자돌림, 매너 테이블에서 계산 안함
+
+            /*  인덱스 순서대로, * = 매너 계산 적용받지 않음
+             *  0 = 일반 단어
+             *  1 = 깐깐에서 사용 불가능한 단어
+             *  2 = 우리말에서 사용 불가능한 단어
+             *  3 = HBW 주제의 단어
+             *  4 = *HBW 주제 외의 어인정 단어
+             */
+            let fi = 0;
+            if (!hbw && (flag & 2)) fi = 4;
+            else if (hbw) fi = 3;
+            else if (flag & 1) fi = 2;
+            else if (flag > 3) fi = 1;
+
+            if (!(flag & 64)) { // 쿵쿵따 전용 단어 스킵
+
+                if (!newManner.hasOwnProperty(start)) newManner[start] = [getMannerTemplate(5), getMannerTemplate(5)];
+                if (!newManner.hasOwnProperty(last)) newManner[last] = [getMannerTemplate(5), getMannerTemplate(5)];
+
+                newManner[start][1][fi].push(data);
+                newManner[last][0][fi].push(data);
+            }
+
+            if (_id.length > 3) continue; // 2, 3글자일때는 쿵쿵따 테이블 기록
+
+            let i = 0;
+            if (_id.length == 2) i = 1; // 3232용 2글자 기록
+
+            if (!newSpcManner.hasOwnProperty(last)) newSpcManner[last] = [getMannerTemplate(5), getMannerTemplate(5)];
+            newSpcManner[last][i][fi].push(data);
+
+        }
+
+        SUBMIT_WORD_CACHE['ko'] = newCache;
+        MANNER_CACHE['ko'] = newManner;
+        SPC_MANNER_CACHE['ko'] = newSpcManner;
+        THEME_CACHE['ko'] = newTheme;
+
+        IOLog.info(`${Object.keys(SUBMIT_WORD_CACHE['ko']).length} 개의 한국어 단어 데이터를 메모리에 불러왔습니다.`)
+        IOLog.debug(`${Object.keys(MANNER_CACHE['ko']).length} 개의 한국어 일반 매너 데이터를 메모리에 불러왔습니다.`)
+        IOLog.debug(`${Object.keys(SPC_MANNER_CACHE['ko']).length} 개의 한국어 특수 매너 데이터를 메모리에 불러왔습니다.`)
+        IOLog.debug(`${Object.keys(THEME_CACHE['ko']).length} 개의 한국어 주제 데이터를 메모리에 불러왔습니다.`)
+    });
+
+    kkutu['en'].find(['_id', ENG_ID]).on($res => {
+        let newCache = {};
+        let newManner = {};
+        let newSpcManner = {};
+        let newTheme = {};
+        for (let resIndex in $res) {
+            const data = $res[resIndex];
+            const _id = data['_id'];
+            const flag = data['flag'];
+            const theme = data['theme'].split(',');
+            
+            newCache[_id] = data;
+
+            // 주제별 캐싱, 선택 불가능 주제는 기록하지 않음
+            for (let t of theme) {
+                if (IJP_EXCEPT.includes(t)) continue;
+                if (!newTheme.hasOwnProperty(t)) newTheme[t] = [];
+                newTheme[t].push(data);
+            }
+
+            if (!_id.match(ENG_ID)) continue; // 클래식에서 사용 불가능한 단어
+
+            if (_id.length < 2) continue; // 끝말/앞말
+            const start = _id.charAt(_id.length-1); // 시작하는 글자
+            const last = _id.charAt(); // 끝나는 글자
+
+            /*  인덱스 순서대로, * = 매너 계산 적용받지 않음
+             *  0 = 일반 단어
+             *  1 = *어인정 단어
+             */
+            let fi = 0;
+            if (flag & 2) fi = 1;
+
+            if (!newManner.hasOwnProperty(start)) newManner[start] = [getMannerTemplate(2), getMannerTemplate(2)];
+            if (!newManner.hasOwnProperty(last)) newManner[last] = [getMannerTemplate(2), getMannerTemplate(2)];
+
+            if (start != last) { // 글자돌림 제외
+                newManner[start][1][fi].push(data);
+                newManner[last][0][fi].push(data);
+            }
+
+            if (_id.length < 4) continue; // 4글자 이상이면 끄투 규칙용 데이터도 저장
+            const kkchar = _id.slice(0,3);
+            const kksub = _id.slice(0,2);
+
+            if (!newSpcManner.hasOwnProperty(kkchar)) newSpcManner[kkchar] = getMannerTemplate(2);
+            if (!newSpcManner.hasOwnProperty(kksub)) newSpcManner[kksub] = getMannerTemplate(2);
+
+            newSpcManner[kkchar][fi].push(data);
+            newSpcManner[kksub][fi].push(data);
+        }
+
+        SUBMIT_WORD_CACHE['en'] = newCache;
+        MANNER_CACHE['en'] = newManner;
+        SPC_MANNER_CACHE['en'] = newSpcManner;
+        THEME_CACHE['en'] = newTheme;
+
+        IOLog.info(`${Object.keys(SUBMIT_WORD_CACHE['en']).length} 개의 영어 단어 데이터를 메모리에 불러왔습니다.`)
+        IOLog.debug(`${Object.keys(MANNER_CACHE['en']).length} 개의 영어 일반 매너 데이터를 메모리에 불러왔습니다.`)
+        IOLog.debug(`${Object.keys(SPC_MANNER_CACHE['en']).length} 개의 영어 특수 매너 데이터를 메모리에 불러왔습니다.`)
+        IOLog.debug(`${Object.keys(THEME_CACHE['en']).length} 개의 영어 주제 데이터를 메모리에 불러왔습니다.`)
+    });
+};
+
+export function refreshShopcache () {    
+    IOLog.info('아이템 데이터 갱신을 시작합니다...')
+
+    kkutu_shop.find().on(function ($res) {
+        let newCache = {};
+
+        $res.forEach(function (item) {
+            newCache[item._id] = item;
+        });
+
+        shop = newCache;
+
+        IOLog.info(`${Object.keys(shop).length} 개의 아이템 데이터를 메모리에 불러왔습니다.`)
+    });
+};
