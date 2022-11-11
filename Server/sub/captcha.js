7844e84943b5a3e1984634180f5cefe773e880fb
KKuTuIO <system@kkutu.io>
2022-11-11 08:10:18 +0000

    최신 게임 서버 소스 코드 업데이트

--- Server/sub/captcha.js
+++ Server/sub/captcha.js
@@ -2,7 +2,7 @@ import fetch, { FormData } from 'node-fetch'
 import GLOBAL from "./global.json" assert { type: "json" };
 
 export function verifyCaptcha (responseToken, remoteIp, callback) {
-    // const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${GLOBAL.CAPTCHA_SECRET_KEY}&response=${responseToken}&remoteip=${remoteIp}`;
+    /* const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${GLOBAL.CAPTCHA_SECRET_KEY}&response=${responseToken}&remoteip=${remoteIp}`; */
     const verifyUrl = `https://challenges.cloudflare.com/turnstile/v0/siteverify`;
     const formData = new FormData();
     formData.set('secret', GLOBAL.CAPTCHA_SECRET_KEY);
