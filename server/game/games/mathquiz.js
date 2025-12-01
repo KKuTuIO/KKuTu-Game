035495c3e45cddebd02364c54397b1365d8040c4
KKuTuIO <system@kkutu.io>
2025-12-01 04:03:49 +0000

    최신 게임 서버 소스 코드 업데이트

--- server/game/games/mathquiz.js
+++ server/game/games/mathquiz.js
@@ -27,11 +27,12 @@ let MAX_NUMBER = 10;
 export function getTitle() {
     let R = new Tail();
     let my = this;
+    let i, j;
 
     let data = [];
-    for (let i = 0; i < my.round; i++) {
+    for (i = 0; i < my.round; i++) {
         let arr = [];
-        for (let j = 0; j < LIST_LENGTH; j++) {
+        for (j = 0; j < LIST_LENGTH; j++) {
             arr.push(generateMathProblem());
         }
         data.push(arr);
@@ -196,13 +197,15 @@ export function getScore(problem, answer, isCorrect) {
 
 function calculateDifficulty(problem) {
     const expression = problem.split('=')[0].trim();
-    const operator = expression.split(' ')[1];
+    const [leftStr, operator, rightStr] = expression.split(' ');
+    const left = parseInt(leftStr);
+    const right = parseInt(rightStr);
     
     switch (operator) {
-        case '＋': return 1;
-        case '－': return 1.2;
-        case '×': return 1.4;
-        case '÷': return 1.6;
+        case '+': return 1; break;
+        case '-': return 1.2; break;
+        case '*': return 1.4; break;
+        case '/': return 1.6; break;
         default: return 1;
     }
 }
